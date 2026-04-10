12c7976dae0520c71f931d29ec9ed162d7b5d38a
Ananya-jainn <ananyaajain08@gmail.com>
2026-04-11 00:53:45 +0530

    inheritance and static

---
+++ Learning_js/inheritance.js
@@ -0,0 +1,47 @@
+class User{
+    constructor(username){
+        this.username = username
+    }
+    logMe(){
+        console.log(`USERNAME is ${this.username}`);
+    }
+}
+
+class teacher extends User {
+    constructor(username,email,pass){
+        super(username)
+        this.email = email
+        this.pass = pass
+        
+    }
+    addCourse(){
+        console.log(`a new course was added by ${this.username}`);
+    }
+}
+const barbie = new teacher("barbie","@gmail","12345667889")
+barbie.addCourse()
+
+const shinchan = new User("shin+can")
+shinchan.logMe()
+
+console.log(barbie===shinchan);
+console.log(barbie instanceof teacher);
+
+
+//Static properties
+
+class UserN{
+    constructor(usernames){
+        this.usernames = usernames
+    }
+
+    loggin(){
+        console.log(`usernames : ${this.usernames}`);
+    }
+
+    createId(){
+        return `123`
+    }
+}
+const ananya = new UserN("ananya")
+console.log(ananya.createId())
